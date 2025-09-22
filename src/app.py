b77a77d23d072e768cf8530eaa95b3602f9fe76a
PrachponleuUch <ponleuvuthy@gmail.com>
2025-09-22 14:08:46 +1000

    auch - 1st commit

---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route("/api/welcome", methods=["GET"])
+def welcome():
+    return jsonify({
+        "message": "Welcome to the Flask API!",
+        "hostname": socket.gethostname(),
+        "deployed_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    })
+    
+@app.route("/", methods=["GET"])
+def health():
+    return jsonify({"status": "OK"}, 200)
+
+if "__name__" == "__main__":
+    app.run(host="0.0.0.0")
