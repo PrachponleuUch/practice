9ca5f817ffa5ee479f16d00d4015fb3948697bd7
PrachponleuUch <ponleuvuthy@gmail.com>
2025-09-27 13:38:13 +1000

    PracticeApp 1st commit

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ import socket
 
 app = Flask(__name__)
 
-@app.route("/api/welcome", methods=["GET"])
+@app.route("/api/v1/welcome", methods=["GET"])
 def welcome():
     return jsonify({
         "message": "Welcome to the Flask API!",
@@ -12,9 +12,9 @@ def welcome():
         "deployed_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     })
     
-@app.route("/", methods=["GET"])
+@app.route("/api/v1/health", methods=["GET"])
 def health():
     return jsonify({"status": "OK"}, 200)
 
-if "__name__" == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=1111)
